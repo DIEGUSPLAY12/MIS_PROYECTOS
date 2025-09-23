031bd5f0ee5a40fb663e776890fd5812b0bf76cb
Diego <diego.garcia@gracia.lasalle.cat>
2025-09-23 18:03:44 +0200

    error ortográfico

--- trabajos/Desenvolupament web en entorn client/javascript/ex2/ex2.js
+++ trabajos/Desenvolupament web en entorn client/javascript/ex2/ex2.js
@@ -1,9 +1,9 @@
-const imputs = document.getElementById('imputs');
+const inputs = document.getElementById('imputs');
 const boton = document.getElementById('boton');
 const mensaje = document.getElementById('mensaje');
 
 boton.onclick = function() {
-    if (imputs.value === "") {
+    if (inputs.value === "") {
         mensaje.textContent = 'No has introducido ningún valor';
     } else {
         mensaje.textContent = 'Has sido correcatmente registrado';
